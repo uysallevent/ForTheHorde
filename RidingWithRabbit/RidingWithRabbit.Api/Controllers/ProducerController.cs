30b369b13f20b108d7912f97381337dfdb591678
levent.uysal <levent.uysal@algomedi.com>
2021-09-01 02:08:14 +0300

    Riding with rabbit init

---
+++ RidingWithRabbit/RidingWithRabbit.Api/Controllers/ProducerController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RidingWithRabbit.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult SendMessageToQue()
+        {
+            try
+            {
+                var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = connectionFactory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "commands",
+                                             durable: false,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null);
+
+                        var messages = new List<string>{
+                                "Riding With The king",
+                                "Key To The HighWay",
+                                "Marry You",
+                                "Three O'Clock Blues"
+                            };
+
+                        foreach (var message in messages)
+                        {
+                            var body = Encoding.UTF8.GetBytes(message);
+                            channel.BasicPublish(exchange: "",
+                                             routingKey: "commands",
+                                             basicProperties: null,
+                                             body: body);
+                            Console.WriteLine($"'{message}' has sent");
+                            Task.Delay(800);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Opppps. Exception => {Environment.NewLine} {JsonConvert.SerializeObject(ex)}");
+            }
+            return Ok("Messages has sent to que");
+        }
+    }
+}
